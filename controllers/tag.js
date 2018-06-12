a49f8b1e2b2cd6162b1e20ea975005307cd61d95
brian <baichengwei1@126.com>
2018-06-12 15:28:56 +0800

    添加标签

---
+++ controllers/tag.js
@@ -0,0 +1,16 @@
+'use strict'
+
+import TagModel from '../models/tag'
+import ArticleModel from '../models/article'
+import BaseComponent from '../prototype/baseComponent'
+import formidable from 'formidable'
+
+
+class Tag extends BaseComponent{
+	constructor(){
+		super()
+	}
+	async putTag(req,res,next){
+		
+	}
+}
